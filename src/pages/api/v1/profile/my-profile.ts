e193597de88a0c738fb4a64f79ab41b299e7a43c
Davidhero <owogogahhero@outlook.com>
2023-08-02 20:07:35 +0100

    major re-write to profile

--- src/pages/api/v1/profile/my-profile.ts
+++ src/pages/api/v1/profile/my-profile.ts
@@ -1,11 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { ApiResponseDto } from "dto/apiResponseDto";
-import jwt from 'jsonwebtoken';
 import { ProfileDto } from "dto/rentn.dto";
 import { prisma } from "config/prisma.connect";
 import { findRentn } from "lib/check.db";
 import { Secret, verify } from "jsonwebtoken";
-import { uuid } from "uuidv4";
 
 export default async function handler (
     req: NextApiRequest,
@@ -24,79 +22,48 @@ export default async function handler (
           authorization,
           process.env.ACCESS_TOKEN_SECRET as Secret
         )
-        // const token: any = authorization?.split('')[1]
-        // console.log(token, "this is the token ::::")
-        // const decode: any = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as Secret )
-        // console.log(decode, "this is the decode :::")
         const {id, email } = payload as { id: string, email: string }
-
-        console.log(email)
-        // ensure that the user exist
         const userExist = await findRentn(email)
         if(!userExist){
           return res.status(404).json({
             message: "oops, who are you? not allowed to make such request",
           });
         }
-        const profileString = uuid()
         const profileData: ProfileDto = req.body;
-        // const createProfile = await prisma.profile.create({
+        if(profileData.role === 'agent') {
+          const createAgent = await prisma.agent.create({
+            data: {
+              firstName: profileData.firstName,
+              lastName: profileData.lastName,
+              gender: profileData.gender,
+              address: profileData.address,
+              phoneNumber: profileData.phoneNumber,
+              role: profileData.role,
+            }
+          })
+          const response: ApiResponseDto = {
+            statusCode: 201,
+            data: createAgent,
+            date: new Date(),
+            url: req.url,
+            message: "Profile completed successfully",
+          };
+          res.status(201).json(response);
+        }
+        // if(profileData.role === 'user') {
+        //   const createUser = await prisma.users.create({
         //     data: {
-        //         firstName: profileData.firstName,
-        //         lastName: profileData.lastName,
-        //         gender: profileData.gender,
-        //         role: profileData.role,
-        //         phoneNumber: profileData.phoneNumber,
+
         //     }
-        // })
-        // switch (profileData.role) {
-        //     case 'Agent':
-        //       await prisma.agent.create({
-        //         data: {
-        //           profile: {
-        //             connect: {
-        //               profileId: createProfile.profileId,
-        //             },
-        //           },
-        //         },
-        //       });
-        //       break;
-        //     case 'User':
-        //       await prisma.users.create({
-        //         data: {
-        //           profile: {
-        //             connect: {
-        //               profileId: createProfile.profileId,
-        //             },
-        //           },
-        //         },
-        //       });
-        //       break;
-        //     case 'Admin':
-        //       await prisma.admin.create({
-        //         data: {
-        //           profile: {
-        //             connect: {
-        //               profileId: createProfile.profileId,
-        //             },
-        //           },
-        //         },
-        //       });
-        //       break;
-        //     default:
-        //       res.status(401).send({
-        //         message: 'your user role is not defined...'
-        //       })
-        //     break;
+        //   })
         // }
-        // const response: ApiResponseDto<any> = {
-        //   statusCode: 201,
-        //   // data: createProfile,
-        //   date: new Date(),
-        //   url: req.url,
-        //   message: "Profile completed successfully",
-        // };
-        res.status(201).json("you have tried");
+        // if(profileData.role === 'admin') {
+        //   const createAdmin = await prisma.admin.create({
+        //     data: {
+
+        //     }
+        //   })
+        // }
       } catch (error: any) {
         console.error(error);
         res.status(500).json({
