f4fb3fabb99fb70d94103c7b9d5d068616b22ed4
Davidhero <owogogahhero@outlook.com>
2023-08-12 04:28:00 +0100

    refactor: minor changes

--- lib/auth.token.ts
+++ lib/auth.token.ts
@@ -1,7 +1,7 @@
 import { Secret, sign, verify, JwtPayload } from 'jsonwebtoken'
 
 export const createAccessToken = (id: string, email: string, role?: string): string => {
-  const expiresIn = "1d"; // 24 hours
+  const expiresIn = "2d"; // 48 hours
   return sign(
     { id: id, email: email, role: role },
     process.env.ACCESS_TOKEN_SECRET as Secret,
