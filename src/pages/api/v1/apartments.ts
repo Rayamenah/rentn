15a5b9f134c1534c2f42c53942f312af8a268c31
Davidhero <owogogahhero@outlook.com>
2023-11-02 13:41:22 +0100

    refactor: lint changes

--- src/pages/api/v1/apartments.ts
+++ src/pages/api/v1/apartments.ts
@@ -1,26 +1,26 @@
-import { apartmentsPaginationHelper } from "lib/paginator";
-import { NextApiRequest, NextApiResponse } from "next";
+import { apartmentsPaginationHelper } from 'lib/paginator'
+import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-){
-    try {
-        const { pageNumber } = req.body;
-        const paginationResult = await apartmentsPaginationHelper(
-          Number(pageNumber)
-        );
-        if (!paginationResult) {
-          return res.status(404).send({
-            message: "No apartments found",
-          });
-        }
-        return res.status(200).send(paginationResult);
-    } catch (error: any) {
-        console.error("Error while fetching apartments:", error);
-        return res.status(500).send({
-          message: "Internal server error",
-          error: error.message,
-        });
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const { pageNumber } = req.body
+    const paginationResult = await apartmentsPaginationHelper(
+      Number(pageNumber)
+    )
+    if (!paginationResult) {
+      return res.status(404).send({
+        message: 'No apartments found',
+      })
     }
+    return res.status(200).send(paginationResult)
+  } catch (error: any) {
+    console.error('Error while fetching apartments:', error)
+    return res.status(500).send({
+      message: 'Internal server error',
+      error: error.message,
+    })
+  }
 }
--- src/pages/api/v1/apartments/getByAgent.ts
+++ src/pages/api/v1/apartments/getByAgent.ts
@@ -1,85 +1,93 @@
-import { prisma } from "config/prisma.connect";
-import { Secret, verify } from "jsonwebtoken";
-import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from 'config/prisma.connect'
+import { Secret, verify } from 'jsonwebtoken'
+import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-){
-    try{
-        const { cookies, pageNumber } = req.body
-        const authorization: any = cookies.rentn
-        if(!authorization) {
-            res.status(401).send({
-                message: 'access token unavailable, access not granted'
-            })
-        }
-        const payload = verify(
-            authorization,
-            process.env.ACCESS_TOKEN_SECRET as Secret
-        )
-        const { id, email, role } = payload as { id: string, email: string, role: string}
-        if(role !== 'agent') {
-            return res.status(404).send({
-                message: "sorry, you're not allowed to access this route. contact support",
-            })
-        }
-        const findAgent = await prisma.agent.findUnique({
-            where: {
-                email: email
-            },
-            include: {
-                apartments: true
-            }
-        })
-        if(!findAgent){
-            return res.status(404).send({
-                message: "sorry, profile details not found or this agent does not exist",
-            })
-        }
-        const pageSize = 10
-        const parsedPageNumber = parseInt(pageNumber as string, 10);
-        if (isNaN(parsedPageNumber) || parsedPageNumber < 1) {
-            return res.status(400).send({
-                message: 'Invalid page number. Please provide a valid page number greater than or equal to 1.',
-            });
-        }
-        const totalCount = await prisma.apartment.count()
-        const totalPages = Math.ceil(totalCount/pageSize)
-        const offset = (parsedPageNumber - 1) * pageSize
-        const getApartmentByAgent = prisma.apartment.findMany({
-            where: {
-                agent: {
-                    email: email
-                }
-            },
-            include: {
-                agent: true,
-                images: true
-            },
-            orderBy: {
-                createdAt: 'asc'
-            },
-            take: pageSize,
-            skip: offset,
-        })
-        if (offset >= totalCount) {
-            return res.status(404).send({
-                message: 'Requested page not found. The specified page number is out of bounds.',
-            });
-        }
-        return res.status(200).send({
-            data: getApartmentByAgent,
-            pages: totalPages,
-            count: totalCount,
-            currentPage: pageNumber
-        })
-    }catch(error: any){
-        console.log(error)
-        console.log(error.message)
-        return res.status(500).send({
-            message: 'sever is currently unavailable',
-            data: error.message
-        })
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const { cookies, pageNumber } = req.body
+    const authorization: any = cookies.rentn
+    if (!authorization) {
+      res.status(401).send({
+        message: 'access token unavailable, access not granted',
+      })
     }
+    const payload = verify(
+      authorization,
+      process.env.ACCESS_TOKEN_SECRET as Secret
+    )
+    const { id, email, role } = payload as {
+      id: string
+      email: string
+      role: string
+    }
+    if (role !== 'agent') {
+      return res.status(404).send({
+        message:
+          "sorry, you're not allowed to access this route. contact support",
+      })
+    }
+    const findAgent = await prisma.agent.findUnique({
+      where: {
+        email: email,
+      },
+      include: {
+        apartments: true,
+      },
+    })
+    if (!findAgent) {
+      return res.status(404).send({
+        message:
+          'sorry, profile details not found or this agent does not exist',
+      })
+    }
+    const pageSize = 10
+    const parsedPageNumber = parseInt(pageNumber as string, 10)
+    if (isNaN(parsedPageNumber) || parsedPageNumber < 1) {
+      return res.status(400).send({
+        message:
+          'Invalid page number. Please provide a valid page number greater than or equal to 1.',
+      })
+    }
+    const totalCount = await prisma.apartment.count()
+    const totalPages = Math.ceil(totalCount / pageSize)
+    const offset = (parsedPageNumber - 1) * pageSize
+    const getApartmentByAgent = prisma.apartment.findMany({
+      where: {
+        agent: {
+          email: email,
+        },
+      },
+      include: {
+        agent: true,
+        images: true,
+      },
+      orderBy: {
+        createdAt: 'asc',
+      },
+      take: pageSize,
+      skip: offset,
+    })
+    if (offset >= totalCount) {
+      return res.status(404).send({
+        message:
+          'Requested page not found. The specified page number is out of bounds.',
+      })
+    }
+    return res.status(200).send({
+      data: getApartmentByAgent,
+      pages: totalPages,
+      count: totalCount,
+      currentPage: pageNumber,
+    })
+  } catch (error: any) {
+    console.log(error)
+    console.log(error.message)
+    return res.status(500).send({
+      message: 'sever is currently unavailable',
+      data: error.message,
+    })
+  }
 }
