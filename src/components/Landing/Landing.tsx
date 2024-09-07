808c5b9ac77172417c2df46f9649b361ff734e12
Rayamenah <raymondamenah@gmail.com>
2024-09-07 01:30:52 +0100

    testing deployment

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import Filter from "../filter/filter"
+import Filter from "../filter/filter.tsx"
 const Landing = () => {
     return (
         <section className='mb-10 flex justify-center items-center sm:justify-center flex-col gap-3 md:gap-8'>
